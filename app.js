e53ce9f5009cc49e16b5032c0462ef994e9052ba
Clinton <clinton.langosch@gmail.com>
2021-10-22 17:15:54 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/rsvp/id/:id", (req, res) => {
+  const { id } = req.params;
+  res.send(`Finding RSVP info for: ${id}`);
+});
+
+app.get("/rsvp/id/:id/update", (req, res) => {
+  const { id } = req.params;
+  /*
+  Does ID exist
+  Have they already submitted
+  Get form data
+  Update database
+  */
+  res.send(`Recording RSVP info for: ${id}`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
